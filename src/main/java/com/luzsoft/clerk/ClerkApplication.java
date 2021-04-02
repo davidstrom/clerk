c7f18c97e50c36aded4c6a016714e1687b90bc0e
David Ström <david.strom@callistaenterprise.se>
2021-04-02 11:51:32 +0200

    Initial setup

---
+++ src/main/java/com/luzsoft/clerk/ClerkApplication.java
@@ -0,0 +1,13 @@
+package com.luzsoft.clerk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClerkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClerkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luzsoft/clerk/ClerkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luzsoft.clerk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClerkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
